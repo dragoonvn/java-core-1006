05401aa0bdfcf4bc30b7684fa144ad3d50911b95
dragoonvn <trietvm@hotmail.com>
2020-06-10 21:35:50 +0700

    first git

---
+++ src/lession1/HelloJava.java
@@ -0,0 +1,8 @@
+package lession1;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Triết");
+        System.out.println("Hello every body");
+    }
+}
